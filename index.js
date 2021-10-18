8696d87539f9b0fe8e91590758b3106a585c8bb5
Ishwarya <ishwaryaraman324@gmail.com>
2021-10-18 19:06:03 +0530

    updated dotenv

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const infoRouter=require("./router.js")
 const mongoose=require("mongoose");
 const morgan=require("morgan");
 const cors=require("cors");
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 5001;
+require("dotenv").config();
 //middleware
 app.use(cors());
 app.use(morgan("dev"))
 app.use(express.json())
 //db connection 
-const url="mongodb+srv://ishwarya:uL1J4VSWiQ4vTT4t@cluster0.9ohui.mongodb.net/crudDb?retryWrites=true&w=majority"
+const url=process.env.MONGO_URL;
+// const url="mongodb+srv://ishwarya:uL1J4VSWiQ4vTT4t@cluster0.9ohui.mongodb.net/crudDb?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
    if(!err){
        console.log("DB connected successfully")
@@ -20,5 +22,5 @@ mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
 app.use("/info",infoRouter)
 // listen port
 app.listen(PORT,()=>{
-    console.log("server started on 5000")
+    console.log("server started on ",PORT)
 })
